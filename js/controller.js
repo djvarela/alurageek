05436fc8388fdd7a4edc6bb12fcc580dcc3ae115
Diego J. Varela <73259288+djvarela@users.noreply.github.com>
2022-08-22 23:37:56 -0300

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,5 @@
 
-const listaProductos = ()=> fetch("http://djvarela.github.io/alurageek/db.json").then((respuesta) => respuesta.json())
+const listaProductos = ()=> fetch("https://djvarela.github.io/alurageek/db.json").then((respuesta) => respuesta.json())
 
 
 export const servicios ={
